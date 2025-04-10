70f744860ade43505c2b7c96089b1a83a410d527
Tomas Cascone <tomascascone@gmail.com>
2025-04-10 20:59:02 -0300

    primera entrega 1.1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/REACTJS-Primera-entrega/'  // Asegúrate que coincida con tu repo
 })
